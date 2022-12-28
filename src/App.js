46a502107da28307ce3e4b81ce4130ab25f209bd
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2022-12-28 21:53:49 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './navbar'
+import Home from './home'
 
 function App() {
+
+//   const title = "Welcome to the new Blog";
+//   const likes = 50;
+//  // const person = { name: 'mathai', age: 22};
+
+//   const link = "http://www.google.com"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <div className="content">
+
+        <Home />
+        {/* <h1> {title} </h1>
+        <p>Liked {likes} times </p>
+
+        <p>{ 10 }</p>
+        <p> { 'Marvin' } </p>
+        <p>{ [ 'apples ', ' mangoes ', ' avocados'] }</p>
+        <p>{ Math.random() * 10 } </p>
+
+
+        <a  href = {link}> Google Site </a> */}
+
+
+
+      </div>
     </div>
   );
 }
