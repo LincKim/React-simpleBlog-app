25e11bdb4fb2a8f3f7327e238ab6e54e968af657
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2022-12-28 22:17:01 +0300

    Adding click event to the home component

--- src/home.js
+++ src/home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+    const handleClick = (e) =>{
+        console.log('Hello Mathai', e);
+    }
+
+    const handleClickAgain = (name) =>{
+        console.log(`Hello ${name}`);
+    }
     return (  
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e)=>{
+                handleClickAgain('Jay', e)
+            }}>Click Me again</button>
         </div>
     );
 }
